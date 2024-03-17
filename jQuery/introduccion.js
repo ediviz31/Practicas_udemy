316aeb8ab936940c4f35ce17f3a67add9555eea3
EDI YOVANY VIZCARRA PULIDO <116475640+ediviz31@users.noreply.github.com>
2024-03-17 17:15:30 -0600

    Add files via upload

---
+++ jQuery/introduccion.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$(".parrafo-uno").click(function(){
+		$(this).hide(3000, function(){
+			$("#parrafo-uno").show(3000, function(){
+				$(".comentario").show(3000, function(){
+					$(".saludo").show(3000);
+					$(".saludo").click(function(){
+						$(".bienvenida").show();
+						$(".bienvenida").click(3000, function(){
+							$(this).hide(3000, function(){
+								$("#imagen").show(3000, function(){
+									$(this).hide(3000, function(){
+										$("#foto").show(3000, function(){
+											$(".contenedor").hide(3000, function(){
+												$(".contenedor").show();
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+	$(".parrafo-dos").click(function(){
+		$(this).hide(3000, function(){
+			$("#parrafo-dos").show();
+		});
+	});
+
+	$(".parrafo-tres").click(function(){
+		$(this).hide(3000, function(){
+			$("#parrafo-tres").show();
+		});
+	});
+});
+
+// He creado 3 parrafos donde accedi a cada uno de ellos y les puse una funcion
+// que al darle click a cada uno de ellos desaparecerian
+// y despues de desaparecer mandarian una alerta de que desaparecio uno y el dos y el tres
